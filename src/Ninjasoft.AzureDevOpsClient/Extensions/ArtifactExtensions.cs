91aad418133c908c4d0ef147b3afbede937af7bf
ninjacoder88 <tom@ninjasoft.io>
2024-05-23 10:55:01 -0500

    added artifact extensions

---
+++ src/Ninjasoft.AzureDevOpsClient/Extensions/ArtifactExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Ninjasoft.AzureDevOpsClient.Models;
+
+namespace Ninjasoft.AzureDevOpsClient.Extensions
+{
+    public static class ArtifactExtensions
+    {
+        public static string? GetArtifactSourceReferenceId(this Artifact artifact, string key)
+        {
+            var source = artifact.GetArtifactSourceReference(key);
+            if (source == null)
+                return null;
+
+            return source.Id;
+        }
+
+        public static string? GetArtifactSourceReferenceName(this Artifact artifact, string key)
+        {
+            var source = artifact.GetArtifactSourceReference(key);
+            if (source == null)
+                return null;
+
+            return source.Name;
+        }
+
+        public static ArtifactSourceReference? GetArtifactSourceReference(this Artifact artifact, string key)
+        {
+            if (!artifact.DefinitionReference.TryGetValue(key, out ArtifactSourceReference? artifactSourceReference))
+                return null;
+
+            return artifactSourceReference;
+        }
+    }
+}
