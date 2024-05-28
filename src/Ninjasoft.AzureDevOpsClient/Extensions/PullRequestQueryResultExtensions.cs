62b74c2d953dbb8e084b4e42321525089c6fcb00
ninjacoder88 <tom@ninjasoft.io>
2024-05-28 07:53:35 -0500

    added method to get pull request ids

---
+++ src/Ninjasoft.AzureDevOpsClient/Extensions/PullRequestQueryResultExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Ninjasoft.AzureDevOpsClient.Models;
+
+namespace Ninjasoft.AzureDevOpsClient.Extensions
+{
+    public static class PullRequestQueryResultExtensions
+    {
+        public static IEnumerable<int> ExtractPullRequestIds(this GitPullRequestQuery<PullRequestQueryResult> pullRequestQueryResult)
+        {
+            foreach(var prqr in pullRequestQueryResult.Results)
+            {
+                foreach(var kvp in prqr)
+                {
+                    foreach(var item in kvp.Value)
+                    {
+                        object prIdObj = item["pullRequestId"];
+                        string prIdStr = prIdObj?.ToString() ?? string.Empty;
+                        if (int.TryParse(prIdStr, out var prId))
+                            yield return prId;
+                    }
+                }
+            }
+        }
+    }
+}
