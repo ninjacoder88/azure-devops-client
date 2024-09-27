ad90e832275620243b51ea3eae8f365a065ab0bf
ninjacoder88 <tom@ninjasoft.io>
2024-09-26 22:38:27 -0500

    Added more fields to IdentityRef

--- src/Ninjasoft.AzureDevOpsClient/Models/IdentityRef.cs
+++ src/Ninjasoft.AzureDevOpsClient/Models/IdentityRef.cs
@@ -3,5 +3,13 @@
     public class IdentityRef
     {
         public string DisplayName { get; set; }
+
+        public string UniqueName { get; set; }
+
+        public string Id { get; set; }
+
+        public string Url { get; set; }
+
+        public string ImageUri { get; set; }
     }
 }
