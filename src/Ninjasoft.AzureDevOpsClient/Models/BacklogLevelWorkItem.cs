47680af6aafc6926fea5e5b870624d7f0a84791b
ninjacoder88 <tom@ninjasoft.io>
2025-10-27 13:27:00 -0500

    add new model

---
+++ src/Ninjasoft.AzureDevOpsClient/Models/BacklogLevelWorkItem.cs
@@ -0,0 +1,9 @@
+﻿namespace Ninjasoft.AzureDevOpsClient.Models
+{
+    public class BacklogLevelWorkItem
+    {
+        public string? Rel { get; set; }
+        public WorkItemReference? Source { get; set; }
+        public WorkItemReference? Target { get; set; }
+    }
+}
